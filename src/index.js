28c10b7c46a45cf629a23522abeb6ebba3679ca2
MiMaxRu <m.rudenkov.115@gmail.com>
2022-10-23 14:28:58 +0300

    fix_2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from "react-dom"//Подключение библиотеки ReactDOM
 import App from "./App"//Подключение компонеты App
 ReactDOM.render(<App />, document.getElementById("root"))//Вывод содержимого App на экран
