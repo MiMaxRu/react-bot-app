8de746faa0bfdb1006d07ce6a2814c284f0482aa
MiMaxRu <m.rudenkov.115@gmail.com>
2022-10-23 13:02:28 +0300

    bot_test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { useEffect } from 'react';
 import './App.css'
-const tg = window.Telegram.WebApp;
+const tg = window.Telegram.WebApp;//Подключаем объект взаимодействия с Telegram
 
 function App() {
   useEffect(() => {
-   // tg.redy();
+    tg.ready();// метод позволяет отследить, когда приложение готово к отображению.
   }, [])
 
   const Close = () => {
-    tg.Close()
+    tg.Close()// метод закрывает приложение.
   }
   return (
     <div className='App'>
-      fwefwf
+      <h1>Первый запуск</h1>
+      <img src='https://thumbs.gfycat.com/SoftOptimalAdeliepenguin-size_restricted.gif'></img>
       <button onClick={Close}>Закрыть</button>
     </div>
   )
